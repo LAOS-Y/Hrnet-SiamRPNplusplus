5d982684622640c0dd4e6f0e5fa317a129ab3cd3
LAOS-Y <billyang426@gmail.com>
2020-01-06 02:15:01 +0800

    first commit

---
+++ preprocessing/create_dataset.sh
@@ -0,0 +1,1 @@
+python create_dataset.py --vid-dir ../ILSVRC2015 --output-dir ./VID_2015_RPN++
---
+++ train_hrnet.sh
@@ -0,0 +1,1 @@
+python train.py --data_dir ./preprocessing/VID_2015_RPN++ --exp hrnet --backbone hrnet --gpu 0,3,6,7  --batch_size 96 --num_workers 12  --random_shift_factor 64 --examplar_img_size 127 --search_region_img_size 255
